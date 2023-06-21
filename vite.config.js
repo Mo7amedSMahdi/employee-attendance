33841544cae824899d1a82d38e1f226cd057baee
Mohammed Salah <nawar@MacBook-Air-m1.local>
2023-06-21 13:22:36 +0300

    ✨ feat(vite.config.js): add ViteImageOptimizer plugin to optimize images and viteCompression plugin to compress assets with Brotli algorithm. Also, add DEFAULT_OPTIONS object to configure image optimization options. Finally, add server options to configure host, port and open page.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,81 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
+import viteCompression from 'vite-plugin-compression';
+
+const DEFAULT_OPTIONS = {
+  test: /\.(jpe?g|png|gif|tiff|webp|svg|avif)$/i,
+  exclude: undefined,
+  include: undefined,
+  includePublic: true,
+  logStats: true,
+  svg: {
+    multipass: true,
+    plugins: [
+      {
+        name: 'preset-default',
+        params: {
+          overrides: {
+            cleanupNumericValues: false,
+            removeViewBox: false, // https://github.com/svg/svgo/issues/1128
+          },
+          cleanupIDs: {
+            minify: false,
+            remove: false,
+          },
+          convertPathData: false,
+        },
+      },
+      'sortAttrs',
+      {
+        name: 'addAttributesToSVGElement',
+        params: {
+          attributes: [{ xmlns: 'http://www.w3.org/2000/svg' }],
+        },
+      },
+    ],
+  },
+  png: {
+    // https://sharp.pixelplumbing.com/api-output#png
+    quality: 85,
+  },
+  jpeg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 85,
+  },
+  jpg: {
+    // https://sharp.pixelplumbing.com/api-output#jpeg
+    quality: 85,
+  },
+  tiff: {
+    // https://sharp.pixelplumbing.com/api-output#tiff
+    quality: 85,
+  },
+  // gif does not support lossless compression
+  // https://sharp.pixelplumbing.com/api-output#gif
+  gif: {},
+  webp: {
+    // https://sharp.pixelplumbing.com/api-output#webp
+    lossless: true,
+  },
+  avif: {
+    // https://sharp.pixelplumbing.com/api-output#avif
+    lossless: true,
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    host: 'localhost',
+    port: 3001,
+    open: '/',
+  },
+  plugins: [
+    react(),
+    ViteImageOptimizer({
+      ...DEFAULT_OPTIONS,
+    }),
+    viteCompression({ algorithm: 'brotliCompress' }),
+  ],
+});
